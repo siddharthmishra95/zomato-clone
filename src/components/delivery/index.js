c61e1a615296237820715103d075dc08646a6610
siddharthmishra95 <siiid.mishra@gmail.com>
2022-03-11 13:27:32 +0530

    Adding Casrousel, delivery and brands

--- src/components/delivery/index.js
+++ src/components/delivery/index.js
@@ -1,8 +1,48 @@
 import React from "react";
+import Filters from "../common/filters";
 import "./delivery.css";
+import DeliveryCollections from "./deliveryCollections";
+import TopBrands from "./topBrands";
+
+const deliveryFilters = [
+  {
+    id: 1,
+    icon: <i className="fi fi-rr-settings-sliders absolute-center"></i>,
+    title: "Filters",
+  },
+  {
+    id: 2,
+    title: "Rating: 4.0+",
+  },
+  {
+    id: 3,
+    title: "Safe and Hygienic",
+  },
+  {
+    id: 4,
+    title: "Pure Veg",
+  },
+  {
+    id: 5,
+    icon: <i className="fi fi-rr-apps-sort absolute-center"></i>,
+    title: "Delivery Time",
+  },
+  {
+    id: 6,
+    title: "Great Offers",
+  },
+];
 
 const Delivery = () => {
-  return <div>This is delivery</div>;
+  return (
+    <div>
+      <div className="max-width">
+        <Filters filterList={deliveryFilters} />
+      </div>
+      <DeliveryCollections />
+      <TopBrands />
+    </div>
+  );
 };
 
 export default Delivery;
